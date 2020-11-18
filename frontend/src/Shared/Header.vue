de67baf588749e7a9f5172ce77e104e5bcdd19d1
Matteo Chini <matteo.chini@gmail.com>
2020-11-18 16:56:35 +0100

    header with router

--- frontend/src/Shared/Header.vue
+++ frontend/src/Shared/Header.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
     <t-button>
-    Entra
+                        <router-link to="/campagne" activeClass="active" tag="li"><a>Entra</a></router-link>
 </t-button>
  <t-button>
-    Esci
+                            <router-link to="/campagne" activeClass="active" tag="li"><a>Esci</a></router-link>
+
 </t-button>
  <t-button>
-    Campagne
+                            <router-link to="/campagne" activeClass="active" tag="li"><a>Campagne</a></router-link>
 </t-button>
  <t-button>
-    Info
+                                <router-link to="/info" activeClass="active" tag="li"><a>Info</a></router-link>
+
 </t-button>
  <t-button>
-    Contatti
+                                <router-link to="/contatti" activeClass="active" tag="li"><a>Contatti</a></router-link>
 </t-button>
     </div>
 </template>
