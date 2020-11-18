451d5a9c35d89fc7d51b3bf3705b0e241a3ee51e
Matteo Chini <matteo.chini@gmail.com>
2020-11-18 17:07:14 +0100

    alignment

--- frontend/src/Shared/Header.vue
+++ frontend/src/Shared/Header.vue
@@ -1,29 +1,48 @@
 <template>
-    <div>
-    <t-button>
-                        <router-link to="/campagne" activeClass="active" tag="li"><a>Entra</a></router-link>
-</t-button>
- <t-button>
-                            <router-link to="/campagne" activeClass="active" tag="li"><a>Esci</a></router-link>
+  <div>
+  <div class="flex mb-4">
+  <div class="flex-1 h-12">
+      <t-button>
+      <router-link to="/campagne" activeClass="active" tag="li"
+        ><a>Entra</a></router-link
+      >
+    </t-button></div>
+  <div class="flex-1  h-12">
+   <t-button>
+      <router-link to="/campagne" activeClass="active" tag="li"
+        ><a>Esci</a></router-link
+      >
+    </t-button></div>
+  <div class="flex-1  h-12">
+      <t-button>
+      <router-link to="/campagne" activeClass="active" tag="li"
+        ><a>Campagne</a></router-link
+      >
+    </t-button></div>
+  <div class="flex-1   h-12">
+      <t-button>
+      <router-link to="/info" activeClass="active" tag="li"
+        ><a>Info</a></router-link
+      >
+    </t-button></div>
+  <div class="flex-1   h-12">
+      <t-button>
+      <router-link to="/contatti" activeClass="active" tag="li"
+        ><a>Contatti</a></router-link
+      >
+    </t-button></div>
+    
+</div>
 
-</t-button>
- <t-button>
-                            <router-link to="/campagne" activeClass="active" tag="li"><a>Campagne</a></router-link>
-</t-button>
- <t-button>
-                                <router-link to="/info" activeClass="active" tag="li"><a>Info</a></router-link>
+   
 
-</t-button>
- <t-button>
-                                <router-link to="/contatti" activeClass="active" tag="li"><a>Contatti</a></router-link>
-</t-button>
-    </div>
+
+
+  </div>
 </template>
 <script>
 export default {
-    name: "Header"
-}
+  name: "Header",
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
