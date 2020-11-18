8a8d0be29509e9730f377d830fa53269ef6bca5e
Matteo Chini <matteo.chini@gmail.com>
2020-11-18 15:28:19 +0100

    fake data for campaigns and companies

---
+++ frontend/src/tmp-data/campaigns.js
@@ -0,0 +1,5 @@
+export const campaigns = [
+    { id: "1", title: "Bike2Work", description:"Campagna di sensibilizzazione all'uso della bici per andare al lavoro",startDate:"12-5-2020",endDate:"12-6-2020",active:false,means:["bike"] },
+    { id: "2", title: "Carpooling2Work", description:"Fai carpooling per andare al lavoro",startDate:"4-10-2020",endDate:"12-5-2021",active:true,means:["car"]},
+    { id: "3", title: "Bike2Work", description:"Campagna di sensibilizzazione all'uso della bici per andare al lavoro",startDate:"12-5-2020",endDate:"12-5-2021",active:true,means:["bike"] }
+  ];
---
+++ frontend/src/tmp-data/companies.js
@@ -0,0 +1,5 @@
+export const companies = [
+    { id: "1", name: "Areaderma", logo:"https://www.areaderma.it/fileadmin/templates/areaderma/Images/logo.png",address:"Via dell'Industria 47",email:"areaderma@gmail.com",phone:"11223344",web:"www.areaderma.it" },
+    { id: "2", name: "Dolomiti Energia", logo:"https://www.dolomitienergia.it/Trenta/assets/img/logo.svg",address:"Viale Venezia 2",email:"dolomoti@energia.it",phone:"55667788",web:"https://www.dolomitienergia.it/content/home"  },
+    { id: "3", name: "Sportler", logo:"https://www.areaderma.it/fileadmin/templates/areaderma/Images/logo.png",address:"Via Mantova, 12",email:"sportler@gmail.com",phone:"991010",web:"www.sportler.com"  }
+  ];
