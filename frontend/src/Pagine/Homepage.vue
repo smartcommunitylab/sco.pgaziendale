75ca30a23bc491c79fa5a953a85ee9a85f2fbeff
Matteo Chini <matteo.chini@gmail.com>
2020-11-19 11:01:39 +0100

    new style for home

--- frontend/src/Pagine/Homepage.vue
+++ frontend/src/Pagine/Homepage.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>HomePage</h1>
+  <div class="home flex flex-col lg:flex-row">
+    <div class="flex flex-col ml-8 mt-8 lg:w-6/12 lg:ml-16 lg:mt-16">
+      <div class="text-4xl mt-8 text-white">Aderisci alle tue</div>
+      <div class="text-4xl  text-white font-bold">
+        <h1>Campagne di mobilitá</h1>
+      </div>
+      <div class="text-xl mt-8 text-white">
+        <b>Partecipa </b> campagne di mobilitá proposte dalla tua azienda
+      </div>
+      <div class="text-xl  text-white">
+        <b>Accedi </b> le stesse credenziali utilizzate nell'APP Play&go
+      </div>
+      <t-button class="w-2/12 mt-8 m-auto" variant="green"><p class="text-center w-full"> Entra </p></t-button>
+    </div>
+    <div class="flex flex-col lg:w-4/12 lg:ml-16 lg:mt-16">
+      <img src="@/assets/images/bike.svg" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +25,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.home {
+  background-color: var(--blue);
+  height: 100%;
+  min-height: 90vh;
+}
+</style>
