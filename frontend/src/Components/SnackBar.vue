99a75bfdb2014b3ba784df55adb3512c1bfa9adb
Erich Robbi <erich.robbi@studenti.unitn.it>
2020-12-09 12:01:09 +0100

    snackbar component

---
+++ frontend/src/Components/SnackBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="snackbar border-l-4 p-4">
+    <p class="font-bold">{{ dTitle }}</p>
+    <p>{{ dDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SnackBar",
+  props: {
+    title: String,
+    description: String,
+    color: String,
+  },
+
+  data: function() {
+    return {
+      dTitle: this.title,
+      dDescription: this.description,
+      dColor: this.color,
+    };
+  },
+};
+</script>
+
+<style>
+.snackbar {
+  border-color: #f6993f;
+  background-color: #fff5eb;
+  color: #f6993f;
+  font: bold;
+}
+</style>
