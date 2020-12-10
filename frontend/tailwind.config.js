550398872a6814eabc8443cd259e22430d329d11
Erich Robbi <erich.robbi@studenti.unitn.it>
2020-12-10 11:17:44 +0100

    created colors palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   purge: [],
   
   variants: {},
+
+  theme: {
+    colors: {
+      primary: '#3182ce',
+      primary_light:"#57a5ed",
+      secondary: '#5ab45f',
+      secondary_light: '#6dde72',
+      danger:'#dc3545',
+      warning:'#ffc107',
+      info:'#17a2b8',
+      white:"#FFFFFF"
+    }
+  },
   plugins: [],
 
 
