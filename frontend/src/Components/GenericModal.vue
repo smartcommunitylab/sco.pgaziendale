9d2eae689543d26bc3a1b2958f0d61e0925db7c1
Matteo Chini <matteo.chini@gmail.com>
2020-12-17 09:39:16 +0100

    fix modal centered in large screen

--- frontend/src/Components/GenericModal.vue
+++ frontend/src/Components/GenericModal.vue
@@ -5,7 +5,7 @@
       class="fixed inset-0 w-full h-screen flex items-center justify-center bg-semi-75"
       @click.self="close"
     >
-      <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+      <div class="lg:ml-64 relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
         <button
           aria-label="close"
           class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4"
