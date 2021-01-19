d2a2462f5407ce73dddb6e7705f0c028afd2b0e9
erich-r <robbi.erich@yahoo.com>
2021-01-19 14:37:45 +0100

    functionalities employees

--- console_aziendale/src/components/Infobox.vue
+++ console_aziendale/src/components/Infobox.vue
@@ -119,8 +119,8 @@
       class="bg-white h-screen shadow-md -mt-16"
       v-else-if="currentView == 4"
     >
-      <div class="pt-16">
-        <div class="flex flex-col justify-center text-center pt-16">
+      <div class="">
+        <div class="flex flex-col justify-center text-center ">
           <div class="flex flex-row ml-auto pr-8">
             <span class="mr-2">
               <svg
@@ -149,34 +149,40 @@
             src="@/assets/images/user-profile.png"
             class="object-contain w-1/2 m-auto pb-2"
           />
-          <h1 class="text-2xl pt-2">Employee</h1>
-          <div class="flex flex-col pt-4">
-            <span class="flex items-center justify-center"
-              ><span class="mr-2">
-                <svg
-                  class=" h-12 w-6  text-grey-dark-m-r-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M4 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-                  />
-                </svg> </span
-              >1</span
-            >
-            <span class="flex items-center justify-center"
-              ><span class="mr-2">
-                <svg
-                  class=" h-12 w-6  text-grey-dark-m-r-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M4 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm6 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-                  />
-                </svg> </span
-              >2</span
-            >
+          <h1 class="text-2xl pt-2">
+            {{ showObject.first_name + " " + showObject.last_name }}
+          </h1>
+          <div class="text-sm">
+            <p>{{ showObject.matricola }}</p>
+            <p>{{ showObject.email }}</p>
+          </div>
+
+          <div class="pt-4">
+            <p>partecipa alle seguenti campagne:</p>
+            <ul>
+              <li
+                class="text-primary text-lg"
+                :key="campaign.id"
+                v-for="campaign in showObject.campagne"
+              >
+                {{ campaign.nome }}
+              </li>
+            </ul>
+          </div>
+
+          <div class="flex flex-row pt-4 max-h-36 ">
+            <div class="w-1/3 flex flex-col py-2  bg-primary text-white">
+              <h3 class="text-6xl">128</h3>
+              <span class="text-sm mt-auto pb-2">Viaggi effettuati</span>
+            </div>
+            <div class="w-1/3 flex flex-col py-2  bg-secondary text-white">
+              <h3 class="text-6xl">1242</h3>
+              <span class="text-sm mt-auto pb-2">Km percorsi</span>
+            </div>
+            <div class="w-1/3 flex flex-col py-2  bg-primary text-white">
+              <img src="@/assets/images/cycling.svg" class=" h-20 m-auto " />
+              <span class="text-sm mt-auto pb-2">mezzo preferito</span>
+            </div>
           </div>
         </div>
       </div>
@@ -205,9 +211,9 @@ export default {
       }
     },
 
-    showEmployeeDetails(/* employee */) {
-      if (this.showObject != 1 /** employee */) {
-        //this.showObject = company;
+    showEmployeeDetails(employee) {
+      if (this.showObject != employee) {
+        this.showObject = employee;
         this.currentView = 4;
       } else {
         this.currentView = 0;
