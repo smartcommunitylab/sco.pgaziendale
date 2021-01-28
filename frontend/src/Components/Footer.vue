140b449a3ee6861d5702530c0bdc1f02fcb3f9c9
Matteo Chini <matteo.chini@gmail.com>
2021-01-28 10:34:29 +0100

    fixed footer dimension and navigation

--- frontend/src/Components/Footer.vue
+++ frontend/src/Components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="absolute w-full bg-gray-900 pb-6 ">
-    <div class=" px-4 lg:ml-64">
+    <div class=" bg-primary px-4 lg:ml-64">
       <hr class="mb-6 border-b-1 border-white" />
       <div
         class="flex flex-wrap items-center md:justify-between justify-center"
@@ -23,14 +23,14 @@
         </div>
         <div class="w-full lg:w-4/12 px-4">
           <div
-            class="flex flex-wrap list-none lg:justify-end  justify-center"
+            class="lg:grid lg:grid-cols-2 list-none lg:justify-end  justify-center"
           >
             <div>
-             <img src="@/assets/images/comunefe.jpg" alt="Comune di Ferrara" class="flex h-12 w-auto m-1" @click="openTab('https://www.comune.fe.it/')" />
+             <img src="@/assets/images/comunefe.jpg" alt="Comune di Ferrara" class="  object-contain  h-12 w-auto max-w-32 m-auto" @click="openTab('https://www.comune.fe.it/')" />
 
             </div>
             <div>
-             <img src="@/assets/images/regioneer.jpeg" alt="Regione Emilia Romagna" class="flex max-w-xs h-12 w-auto m-1" @click="openTab('https://www.regione.emilia-romagna.it/')"/>
+             <img src="@/assets/images/regioneer.jpeg" alt="Regione Emilia Romagna" class="  object-contain  h-12 w-auto max-w-32 m-auto" @click="openTab('https://www.regione.emilia-romagna.it/')"/>
             </div>
 
           </div>
@@ -57,5 +57,10 @@ export default {
 footer{
   /* z-index: 999; */
   background-color: #1970b7;
+      clear: both;
+    position: relative;
+    height: 100px;
+    margin-top: -100px;
+
 }
 </style>
