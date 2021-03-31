5979f35bf9fe7a592b6c11c25c2a165ae3574290
Matteo Chini <matteo.chini@gmail.com>
2021-03-31 17:16:18 +0200

    working on stats

--- console_aziendale/src/Pagine/Stat/Stats.vue
+++ console_aziendale/src/Pagine/Stat/Stats.vue
@@ -14,7 +14,7 @@
         <div class="flex flex-col md:flex-row mt-3 justify-stretch lg:flex-col">
           <label for="sub_select">Seleziona una campagna</label>
           <select
-            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none"
+            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none text-primary bg-white"
             name="sub_select"
             id="campaign"
             v-model="selectedCampaign"
@@ -37,10 +37,10 @@
         >
           <label for="sub_select">Seleziona una azienda</label>
           <select
-            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none"
+            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none text-primary bg-white"
             name="sub_select"
             id="campaign"
-            v-model="showFilter.selectedCompany"
+            v-model="selectedCompany"
             @change="changeCompany($event)"
             required
           >
@@ -54,13 +54,13 @@
             </option>
           </select>
         </div>
-        <div class="flex flex-col md:flex-row mt-3 justify-stretch lg:flex-col">
+        <div class="flex flex-col md:flex-row mt-3 justify-stretch lg:flex-col" v-if="role=='ROLE_COMPANY_ADMIN'">
           <label for="sub_select">Seleziona una sede</label>
           <select
-            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none"
+            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none text-primary bg-white"
             name="sub_select"
             id="campaign"
-            v-model="showFilter.selectedSede"
+            v-model="selectedSede"
             @change="changeSede($event)"
             required
           >
@@ -74,7 +74,42 @@
             </option>
           </select>
         </div>
-        <div class="flex flex-col mt-3 justify-stretch">
+        <div class="flex flex-col md:flex-row mt-3 justify-stretch lg:flex-col"  v-if="allEmployees &&allEmployees.items">
+          <label for="sub_select">Seleziona un dipendente</label>
+          <select
+            class="focus:border-blue-600 border-2 p-2 mb-2 md:mb-0 lg:mb-2 flex-1 md:mr-2 lg:mr-0 appearance-none text-primary bg-white"
+            name="sub_select"
+            id="employee"
+            v-model="selectedEmployee"
+            @change="changeEmployee($event)"
+            required
+          >
+            <option disabled value="">Seleziona un dipendente</option>
+            <option
+              v-for="employee in allEmployees.items"
+              :value="employee"
+              :key="employee.id"
+            >
+              {{ employee.name }} {{ employee.surname }}
+            </option>
+          </select>
+        </div>
+         <div class="flex flex-col md:flex-row mt-3 justify-stretch lg:flex-col" >
+          <label for="sub_select">Visualizzazione</label>
+              <div class="flex flex-col items-center justify-center ">
+        <div class="flex flex-col">
+            <label class="inline-flex items-center mt-3">
+                <input type="radio" class="form-radio h-5 w-5 text-primary" value="month" v-model="span"><span class="ml-2 text-gray-700">Mese</span>
+            </label>
+
+            <label class="inline-flex items-center mt-3">
+                <input type="radio" class="form-radio h-5 w-5 text-primary" value="total" v-model="span"><span class="ml-2 text-gray-700">Totale</span>
+            </label>
+        </div>
+        
+              </div>
+         </div>
+        <!-- <div class="flex flex-col mt-3 justify-stretch">
           <div class="flex flex-col mt-3 justify-stretch w-full">
             <label for="sub_select">Dal</label>
 
@@ -143,40 +178,39 @@
               <input type="text" v-model="checkin" class="text-primary w-full" />
             </VueTailwindPicker>
           </div>
-        </div>
+        </div> -->
+              <button
+          type="button"
+          class="btn-close"
+          @click="showStat"
+          aria-label="Close modal"
+        >
+          Mostra statistica
+        </button>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
-import VueTailwindPicker from "vue-tailwind-picker";
+// import VueTailwindPicker from "vue-tailwind-picker";
 export default {
-  components: {
-    VueTailwindPicker,
-  },
+  // components: {
+  //   VueTailwindPicker,
+  // },
   data() {
     return {
       checkin: "",
-      showFilter: {
-        selectedCampaign: null,
-        azienda: null,
-        sede: null,
-        period: {
-          dal: null,
-          al: null,
-        },
-      },
-      sendFilter: {
-        selectedCampaign: null,
-        azienda: null,
-        sede: null,
-        period: {
-          dal: null,
-          al: null,
-        },
-      },
+      span:"month",
+      selectedCompany:"",
+      selectedCampaign:"",
+      selectedEmployee:"",
+      azienda:"",
+      sede:"",
+      dal:"",
+      al:""
     };
   },
   computed: {
@@ -184,19 +218,23 @@ export default {
     ...mapState("campaign", ["allCampaigns"]),
     ...mapState("company", ["allCompanies"]),
     ...mapState("location", ["allLocations"]),
+    ...mapState("employee", ["allEmployees"]),
   },
   created() {
     //default company if AA
-    this.sendFilter.azienda = null;
+    this.azienda = null;
     if (this.role == "ROLE_COMPANY_ADMIN") {
       var company = this.getFirstCompany(this.user);
-      if (company) this.sendFilter.azienda = company.companyId;
+      if (company) this.azienda = company.companyId;
     }
     //get all the campaigns, if azienda is set, filter by it
-    this.getAllCampaigns(this.sendFilter.azienda);
+    this.getAllCampaigns(this.azienda);
 
     if (this.role == "ROLE_ADMIN") this.getAllCompanies();
-    if (this.sendFilter.azienda) this.getAllLocations(this.sendFilter.azienda);
+    if (this.azienda) {
+      this.getAllLocations(this.azienda);
+      this.getAllEmployees(this.azienda);
+    }
   },
   mounted() {
     this.changePage({title: 'Statistiche',
@@ -208,7 +246,8 @@ export default {
     ...mapActions("company", { getAllCompanies: "getAll" }),
     ...mapActions("location", { getAllLocations: "getAll" }),
     ...mapActions("navigation", { changePage: "changePage" }),
-
+    ...mapActions("employee", { getAllEmployees: "getAll" }),
+    ...mapActions("stat",{getStat:"getStat"}),
     getFirstCompany(user) {
       return user.roles.find(function (role) {
         return role.role == "ROLE_COMPANY_ADMIN";
@@ -229,13 +268,22 @@ export default {
     changeCompany(event) {
       //get new data for the new campaign
       console.log(event);
-      console.log(event.target.value)
+      console.log(event.target.value);
+      this.getAllEmployees(this.selectedCompany.id);
     },
     changeSede(event) {
       //get new data for the new campaign
       console.log(event);
       console.log(event.target.value)
     },
+        changeEmployee(event) {
+      //get new data for the new campaign
+      console.log(event);
+      console.log(event.target.value)
+    },
+    showStat(){
+      console.log("getStat and show values");
+    }
   },
 };
 </script>
--- console_aziendale/src/services/stat.services.js
+++ console_aziendale/src/services/stat.services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const userService = {
+export const statService = {
     getStat
 
 };
