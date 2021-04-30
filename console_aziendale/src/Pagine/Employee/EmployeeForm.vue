ad288282c5313e2f5e2b78c1ba6118ae161b5aa5
Matteo Chini <matteo.chini@gmail.com>
2021-04-30 10:05:17 +0200

    fixed null issue with id

--- console_aziendale/src/Pagine/Employee/EmployeeForm.vue
+++ console_aziendale/src/Pagine/Employee/EmployeeForm.vue
@@ -132,7 +132,7 @@ export default {
     }),
     initEmployee() {
       this.employee = {};
-      this.id = "";
+      this.id = null;
       this.name = "";
       this.surname = "";
       this.code = "";
