2be1c0aae056f77524f0e5e57ac6ad6950cf9b78
DimitriMasella <dimitrimasella@gmail.com>
2021-10-15 10:26:51 +0200

    Prove del componente modal per le query stats.

--- console_aziendale/src/components/sidebar/Sidebar.vue
+++ console_aziendale/src/components/sidebar/Sidebar.vue
@@ -34,22 +34,72 @@
             </v-card-actions>
         </v-card>
 
+        <!-- Gestore di inserimento dati -->
         <v-bottom-sheet v-model="sheet">
             <v-sheet
-                class="text-center"
-                height="200px"
+                height="500px"
             >
-                <v-btn
-                class="mt-6"
-                text
-                color="red"
-                @click="sheet = !sheet"
-                >
-                close
-                </v-btn>
-                <div class="py-3">
-                This is a bottom sheet using the controlled by v-model instead of activator
+                <div class="text-center">
+                    <v-btn
+                    class="mt-6"
+                    text
+                    color="red"
+                    @click="sheet = !sheet"
+                    >
+                    close
+                    </v-btn>
                 </div>
+                <form>
+                    <v-row>
+                        <v-col cols="3">
+                            <v-select
+                            v-model="select"
+                            :items="items"
+                            :error-messages="selectErrors"
+                            label="Seleziona una campagna"
+                            required
+                            @change="$v.select.$touch()"
+                            @blur="$v.select.$touch()"
+                            ></v-select>
+                            <v-select
+                            v-model="select"
+                            :items="items"
+                            :error-messages="selectErrors"
+                            label="Seleziona un'azienda"
+                            required
+                            @change="$v.select.$touch()"
+                            @blur="$v.select.$touch()"
+                            ></v-select>
+                            <v-select
+                            v-model="select"
+                            :items="items"
+                            :error-messages="selectErrors"
+                            label="Seleziona il tipo di dato"
+                            required
+                            @change="$v.select.$touch()"
+                            @blur="$v.select.$touch()"
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                    <v-checkbox
+                    v-model="checkbox"
+                    :error-messages="checkboxErrors"
+                    label="Do you agree?"
+                    required
+                    @change="$v.checkbox.$touch()"
+                    @blur="$v.checkbox.$touch()"
+                    ></v-checkbox>
+
+                    <v-btn
+                    class="mr-4"
+                    @click="submit"
+                    >
+                    submit
+                    </v-btn>
+                    <v-btn @click="clear">
+                    clear
+                    </v-btn>
+                </form>
             </v-sheet>
         </v-bottom-sheet>
 
@@ -58,16 +108,86 @@
 
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, email } from 'vuelidate/lib/validators'
+
 export default {
+    mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(10) },
+      email: { required, email },
+      select: { required },
+      checkbox: {
+        checked (val) {
+          return val
+        },
+      },
+    },
+
     props: {
         method: Function,
     },
+
     data(){
         return {
             sheet: false,
             pippo: "ciao",
+
+            name: '',
+            email: '',
+            select: null,
+            items: [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+                'Item 4',
+            ],
+            checkbox: false,
         }
-    }
+    },
+
+    computed: {
+      checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      },
+      selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      },
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.name.required && errors.push('Name is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      },
+    },
+
+    methods: {
+      submit () {
+        this.$v.$touch()
+      },
+      clear () {
+        this.$v.$reset()
+        this.name = ''
+        this.email = ''
+        this.select = null
+        this.checkbox = false
+      },
+    },
 }
 </script>
 
