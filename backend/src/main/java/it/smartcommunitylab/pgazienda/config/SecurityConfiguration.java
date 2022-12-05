9c8597ba591636be88f8001b4c02d5f5b46e32c5
micnori <micnori@users.noreply.github.com>
2022-12-05 16:28:19 +0100

    new spring boot and swagger

--- backend/src/main/java/it/smartcommunitylab/pgazienda/config/SecurityConfiguration.java
+++ backend/src/main/java/it/smartcommunitylab/pgazienda/config/SecurityConfiguration.java
@@ -27,8 +27,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import it.smartcommunitylab.pgazienda.Constants;
 import it.smartcommunitylab.pgazienda.security.jwt.JWTConfigurer;
@@ -57,17 +55,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/test/**");
     }
     
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer() 
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
-            }
-        };
-    }
     
     @Override
     public void configure(HttpSecurity http) throws Exception {
