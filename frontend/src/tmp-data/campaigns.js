257f980b25732ae2a30ccff9b0fd39ee6c4f9195
kazhamiakin <kazhamiakin@gmail.com>
2023-11-06 23:50:37 +0100

    cleanup

--- frontend/src/tmp-data/campaigns.js
+++
@@ -1,5 +0,0 @@
-export const campaigns = [
-    { id: "1", title: "Bike2Work", description:"Campagna di sensibilizzazione all'uso della bici per andare al lavoro",startDate:"2020-5-12",endDate:"2020-6-12",active:false,means:["bike"] },
-    { id: "2", title: "Carpooling2Work", description:"Fai carpooling per andare al lavoro",startDate:"2020-10-4",endDate:"2021-5-12",active:true,means:["car"]},
-    { id: "3", title: "Bike2Work", description:"Campagna di sensibilizzazione all'uso della bici per andare al lavoro",startDate:"2020-5-12",endDate:"2021-5-12",active:true,means:["bike"] }
-  ];
