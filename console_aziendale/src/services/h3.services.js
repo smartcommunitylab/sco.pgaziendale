0ef3de6b3dfb045a5a38cf64d2d8b0c4ca39ec9e
Matteo Chini <matteo.chini@gmail.com>
2026-06-23 14:13:50 +0200

    new env api for ms clarity

--- console_aziendale/src/services/h3.services.js
+++ console_aziendale/src/services/h3.services.js
@@ -1,45 +1,31 @@
 import axios from "axios";
 
+const BASE_URL = process.env.VUE_APP_BASE_URL;
+
 export const h3Service = {
   getDurationMap,
   getUserDepartureMap,
   getTripsMap
 };
 
+function buildParams(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
+  return { territory_id, campaign_id, target_resolution: resolution, min_tracks, h3_cell, is_departure };
+}
+
 function getDurationMap(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
-  return axios.get(`${process.env.VUE_APP_BASE_URL}geo/duck/avg-duration`, {
-    params: {
-      territory_id,
-      campaign_id,
-      target_resolution: resolution,
-      min_tracks,
-      h3_cell,
-      is_departure
-    }
+  return axios.get(`${BASE_URL}${process.env.VUE_APP_H3_AVG_DURATION_API}`, {
+    params: buildParams(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure)
   }).then(res => res.data);
 }
-  function getUserDepartureMap(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
-    return axios.get(`${process.env.VUE_APP_BASE_URL}geo/duck/avg-duration`, {
-      params: {
-        territory_id,
-        campaign_id,
-        target_resolution: resolution,
-        min_tracks,
-        h3_cell,
-        is_departure
-      }
-    }).then(res => res.data);
-  }
-    function getTripsMap(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
-        return axios.get(`${process.env.VUE_APP_BASE_URL}geo/duck/avg-duration`, {
-          params: {
-            territory_id,
-            campaign_id,
-            target_resolution: resolution,
-            min_tracks,
-            h3_cell,
-            is_departure
-          }
-        }).then(res => res.data);
+
+function getUserDepartureMap(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
+  return axios.get(`${BASE_URL}${process.env.VUE_APP_H3_USER_DEPARTURE_API}`, {
+    params: buildParams(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure)
+  }).then(res => res.data);
 }
-  
+
+function getTripsMap(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure) {
+  return axios.get(`${BASE_URL}${process.env.VUE_APP_H3_TRIPS_API}`, {
+    params: buildParams(territory_id, campaign_id, resolution, min_tracks, h3_cell, is_departure)
+  }).then(res => res.data);
+}
